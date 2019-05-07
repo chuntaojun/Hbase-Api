4b386d3833f7a5fccd2fb6293e65efdd0507ff2f
hdumeteor <39825419+hdumeteor@users.noreply.github.com>
2019-05-07 20:54:06 +0800

    Add files via upload
    新闻的方法

--- provider/src/main/java/com/tensor/api/org/service/solr/SolrNewService.java
+++ provider/src/main/java/com/tensor/api/org/service/solr/SolrNewService.java
@@ -11,17 +11,81 @@ import reactor.core.publisher.Mono;
 
 public interface SolrNewService {
 
+ /**
 
-    Mono<ResultData<Boolean>> putNews(News news);
+  * 根据传入的参数将新闻插入数据库 返回是否成功
 
-    Mono<ResultData<JsonObject>> getAllNews();
+  *
 
-    Mono<ResultData<JsonObject>> getNewsByRowKey(String rowKey);
+  * @param news
 
-    Mono<ResultData<JsonObject>> queryNewsByAuthor(String author);
+  * @return
 
-    Mono<ResultData<JsonObject>> queryNewsByTitle(String newTitle);
+  */
 
-    Mono<ResultData<JsonObject>> queryNewsByType(String newType);
-    
+ Mono<ResultData<Boolean>> putNews(News news);
+ /**
+  * 读取全部新闻
+
+  *
+
+  * @return
+
+  */
+
+ Mono<ResultData<JsonObject>> getAllNews(int pagenumber);
+
+
+ /**
+
+  * 根据id读取新闻 返回对应新闻
+
+  *
+
+  * @param id
+
+  * @return
+
+  */
+
+ Mono<ResultData<JsonObject>> getNewsByRowKey(String id);
+
+ /**
+
+  * 作者  查询
+
+  *
+
+  * @param author
+  @param pagenumbwer
+   * @return
+
+  */
+ Mono<ResultData<JsonObject>> queryNewsByAuthor(int pagenumbwer ,String author);
+ /**
+
+  * 标题  查询
+
+  *
+
+  * @param newTitle
+  @param pagenumbwer
+   * @return
+
+  */
+ Mono<ResultData<JsonObject>> queryNewsByTitle(int pagenumbwer,String newTitle);
+
+ /**
+
+  * 分类查询  查询
+
+  *
+
+  * @param newType
+  @param pagenumbwer
+   * @return
+
+  */
+ Mono<ResultData<JsonObject>> queryNewsByType(int pagenumbwer,String newType);
+
 }
